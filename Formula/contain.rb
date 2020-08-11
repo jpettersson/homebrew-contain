75370a1c7e1cbafeb84779396e2cd569627d26c1
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2020-08-11 14:52:01 +0200

    Release contain 0.3.0

--- Formula/contain.rb
+++ Formula/contain.rb
@@ -1,9 +1,9 @@
 class Contain < Formula
   desc "Contain your dev environments"
   homepage "https://github.com/jpettersson/contain"
-  url "https://github.com/jpettersson/contain/archive/0.2.0.tar.gz"
-  version "0.2.0"
-  sha256 "70f5297cff02ec36c58e5d60a5e87cd30aa6d94d87ee0ccff8547efffff9cff7"
+  url "https://github.com/jpettersson/contain/archive/0.3.0.tar.gz"
+  version "0.3.0"
+  sha256 "09309fb2dda34484439d4384703046c353a1e95f178726c4a1af2d082928cf69"
 
   depends_on "rust" => :build
 
